d6ecfbc9f9b23b8ba34492fff064026cf6d70883
arsikurin <arseny.kurin@gmail.com>
2021-07-30 18:07:22 +0300

    enhance clarity.
    add new features
    
    ...

--- main.py
+++ main.py
@@ -6,13 +6,12 @@ import sqlite3
 import requests as rq
 
 from datetime import date
-from telethon import Button, TelegramClient, events
+from telethon import TelegramClient, events
 from telethon.errors import MessageDeleteForbiddenError
 from telethon.errors.common import MultiError
-from essential import LOGIN_URL_LOCAL, API_ID, API_HASH, BOT_TOKEN, init_user_sql, is_inited, is_inited_sql, \
-    send_certain_day_schedule, send_greeting, send_main_page, send_holidays, \
-    to_main_page, to_schedule_page, to_homework_page, to_personal_page, to_certain_day_schedule_page, \
-    to_certain_day_homework_page, \
+from essential import API_ID, API_HASH, BOT_TOKEN, init_user_sql, is_inited, is_inited_sql, \
+    send_certain_day_schedule, send_greeting, send_main_page, send_holidays, send_init_message, \
+    to_main_page, to_schedule_page, to_homework_page, to_certain_day_schedule_page, to_certain_day_homework_page, \
     receive_token, receive_student_id, get_message_sql, update_data, set_message_sql
 
 "https://s-api.letovo.ru/api/students/54405"
@@ -32,16 +31,7 @@ with rq.Session() as s:
         await send_greeting(client=client, sender=sender)
 
         if not is_inited(chat_id=chat_id):
-            await client.send_message(entity=sender,
-                                      message=f"I will help you access your schedule via Telegram.\n"
-                                              "Initially, you should provide your login and password to"
-                                              " [Letovo Analytics](s.letovo.ru).\n  "
-                                              f'To do that click the button below\n'
-                                              "__After logging into your account, click Start button again__",
-                                      parse_mode="md",
-                                      buttons=[
-                                          Button.url(text="Log in", url=f'{LOGIN_URL_LOCAL + "?chat_id=" + chat_id}')
-                                      ])
+            await send_init_message(client=client, sender=sender)
 
             if not is_inited_sql(chat_id=chat_id, conn=conn, c=c):
                 init_user_sql(chat_id=chat_id, conn=conn, c=c)
@@ -49,44 +39,44 @@ with rq.Session() as s:
 
         await send_main_page(client=client, sender=sender)
 
-        update_data(chat_id=chat_id, token=t if (t := receive_token(s, chat_id)) else "error")
-        update_data(chat_id=chat_id, student_id=t if (t := receive_student_id(s, chat_id)) else "error")
-        set_message_sql(chat_id=str(sender.id), message_id=event.message.id, conn=conn, c=c)
-        # set_message_sql(chat_id=str(event.original_update.user_id), message_id=event.query.msg_id - 1, conn=conn, c=c)
+        update_data(chat_id=chat_id, token=receive_token(s, chat_id))
+        update_data(chat_id=chat_id, student_id=receive_student_id(s, chat_id))
+        set_message_sql(chat_id=chat_id, message_id=event.message.id + 3, conn=conn, c=c)
 
 
     @client.on(events.CallbackQuery(pattern=r"(?i).*schedule"))
     async def schedule(event):
-        chat_id = str(event.original_update.user_id)
+        chat_id: str = str(event.original_update.user_id)
         if event.data == b"todays_schedule":
-            await send_certain_day_schedule(int(date.today().strftime("%w")) - 1, event, client, s, chat_id)
+            await send_certain_day_schedule(day=int(date.today().strftime("%w")) - 1,
+                                            event=event, client=client, s=s, chat_id=chat_id)
 
         elif event.data == b"entire_schedule":
-            await send_certain_day_schedule(-10, event, client, s, chat_id)
+            await send_certain_day_schedule(day=-10, event=event, client=client, s=s, chat_id=chat_id)
 
         elif event.data == b"monday_schedule":
-            await send_certain_day_schedule(0, event, client, s, chat_id)
+            await send_certain_day_schedule(day=0, event=event, client=client, s=s, chat_id=chat_id)
 
         elif event.data == b"tuesday_schedule":
-            await send_certain_day_schedule(1, event, client, s, chat_id)
+            await send_certain_day_schedule(day=1, event=event, client=client, s=s, chat_id=chat_id)
 
         elif event.data == b"wednesday_schedule":
-            await send_certain_day_schedule(2, event, client, s, chat_id)
+            await send_certain_day_schedule(day=2, event=event, client=client, s=s, chat_id=chat_id)
 
         elif event.data == b"thursday_schedule":
-            await send_certain_day_schedule(3, event, client, s, chat_id)
+            await send_certain_day_schedule(day=3, event=event, client=client, s=s, chat_id=chat_id)
 
         elif event.data == b"friday_schedule":
-            await send_certain_day_schedule(4, event, client, s, chat_id)
+            await send_certain_day_schedule(day=4, event=event, client=client, s=s, chat_id=chat_id)
 
         elif event.data == b"saturday_schedule":
-            await send_certain_day_schedule(5, event, client, s, chat_id)
+            await send_certain_day_schedule(day=5, event=event, client=client, s=s, chat_id=chat_id)
 
 
     @client.on(events.CallbackQuery(pattern=r"(?i).*homework"))
     async def homework(event):
-        chat_id = str(event.original_update.user_id)
-        if event.data == b"todays_homework":
+        chat_id: str = str(event.original_update.user_id)
+        if event.data == b"tomorrows_homework":
             await event.answer("Under development", alert=False)
             # await send_certain_day_homework(int(date.today().strftime("%w")) - 1, event, client, s, chat_id)
 
@@ -124,11 +114,6 @@ with rq.Session() as s:
         await to_main_page(event=event)
 
 
-    @client.on(events.CallbackQuery(data=b"personal_page"))
-    async def personal_data(event):
-        await to_personal_page(event=event)
-
-
     @client.on(events.CallbackQuery(data=b"schedule_page"))
     async def schedule_page(event):
         await to_schedule_page(event=event)
@@ -151,7 +136,7 @@ with rq.Session() as s:
 
     @client.on(events.CallbackQuery(data=b"holidays"))
     async def holidays(event):
-        await event.delete()
+        await event.answer("", alert=False)
         await send_holidays(client=client, sender=await event.get_sender())
 
 
@@ -161,29 +146,22 @@ with rq.Session() as s:
 
 
     @client.on(events.NewMessage())
-    async def handle_messages(event):
+    async def handle_new_messages(event):
         sender = await event.get_sender()
         chat_id: str = str(sender.id)
-        if event.message.message.lower() == "clear previous":
-            await send_greeting(client=client, sender=sender)
 
-            msg_ids = [i for i in range(get_message_sql(chat_id=chat_id, conn=conn, c=c), event.message.id)]
+        if event.message.message.lower() == "clear previous":
+            msg_ids: list[int] = [i for i in range(get_message_sql(chat_id=chat_id, conn=conn, c=c), event.message.id)]
             await event.delete()
             try:
                 await client.delete_messages(entity=sender, message_ids=msg_ids)
             except MultiError or MessageDeleteForbiddenError:
                 pass
 
-            await send_main_page(client=client, sender=sender)
-
         elif event.message.message.lower() != "start":
             await event.delete()
-            # await client.delete_messages(await event.get_sender(), event.message.id)
 
 
     if __name__ == "__main__":
         client.start(bot_token=BOT_TOKEN)
         client.run_until_disconnected()
-
-    # msg_id = event.original_update.msg_id
-    # entity = event.query.peer
