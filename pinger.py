5e99fd9dc4ab2245061b83b1ce9afaae4039cb4f
arsikurin <arseny.kurin@gmail.com>
2021-09-08 21:03:03 +0300

    add pinger.
    add pinger
    
    add pinger

---
+++ pinger.py
@@ -0,0 +1,14 @@
+import requests as rq
+import logging as log
+from essential import (
+    MAIN_URL_API
+)
+
+
+def main():
+    response = rq.get(MAIN_URL_API)
+    log.debug(f"I am NOT sleeping {response.status_code}")
+
+
+if __name__ == "__main__":
+    main()
