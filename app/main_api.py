4161b173d89c116e458ee0c3e768c9f66fcfbd24
Arseny Kurin <58228813+arsikurin@users.noreply.github.com>
2022-01-25 08:53:19 +0300

    Transfer files
    **do not use for production**

--- app/main_api.py
+++ app/main_api.py
@@ -12,7 +12,7 @@ from app.api.endpoints import login_router
 from config import settings
 
 app = FastAPI(
-    title=settings().title,
+    title=settings().title_api,
 )
 
 app.mount("/static", StaticFiles(directory="./app/static"), name="static")
--- config.py
+++ config.py
@@ -1,17 +1,12 @@
 import functools as ft
-import typing
 from zoneinfo import ZoneInfo
 
 import orjson
-
-try:
-    # set environment variables if on local machine
-    from debug import include_secrets
-
-    include_secrets()
-except ImportError:
-    include_secrets = typing.Any
+from dotenv import load_dotenv, find_dotenv
 from pydantic import BaseSettings, HttpUrl, PostgresDsn, BaseConfig
+
+# Use vars from local .env file if on local machine
+load_dotenv(find_dotenv(".env.development.local", raise_error_if_not_found=False))
 
 # fpath = os.path.join(os.path.dirname(__file__), 'utils')
 # sys.path.append(fpath)
@@ -24,8 +19,8 @@ class AppSettings(BaseSettings):
     URL_MAIN_LETOVO: HttpUrl = "https://s.letovo.ru"
     URL_MAIN_API: HttpUrl = "https://letovo-analytics-api.herokuapp.com/"
     URL_MAIN_LOCAL: HttpUrl = "https://letovo-analytics.web.app/"
-    URL_LOGIN_LOCAL: HttpUrl = "https://letovo-analytics.web.app/login"
     URL_LOGIN_LETOVO: HttpUrl = "https://s-api.letovo.ru/api/login"
+    URL_LOGIN_LOCAL: HttpUrl = "https://letovo-analytics.web.app/login"
     GOOGLE_FS_KEY: str
     GOOGLE_API_KEY: str
     TG_API_ID: int
@@ -35,7 +30,7 @@ class AppSettings(BaseSettings):
     WEB_CONCURRENCY: int = 4
     PORT: int = 8084
     debug: bool = False
-    title: str = "Letovo Analytics Bot API"
+    title_api: str = "Letovo Analytics Bot API"
     favicon_path: str = "app/static/images/icons/api-icon.png"
     timezone: ZoneInfo = ZoneInfo("Europe/Moscow")
 
@@ -47,6 +42,6 @@ class AppSettings(BaseSettings):
 @ft.cache
 def settings() -> AppSettings:
     """
-    `settings.cache_clear()` if wanna dump cache
+    `settings.cache_clear()` to dump cache
     """
     return AppSettings()
