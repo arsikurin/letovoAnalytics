7b62643a3ddcadc5a4e4e7e91a0b7cbaf09992d3
arsikurin <2024kurin.av@student.letovo.ru>
2022-01-28 20:29:07 +0300

    Major Release
    v8 GitHub actions
    enhance docs coverage
    refactor marks parsing
    add v1 teachers schema
    
    +minor bugfixes

--- app/dependencies/utils.py
+++ app/dependencies/utils.py
@@ -6,13 +6,18 @@ def run_immediately(function: typing.Callable[..., typing.Any]):
     """
     Decorator used to execute asynchronous functions more conveniently
     """
+
     asyncio.run(function())
 
 
 async def run_sequence_ag(*functions: typing.Awaitable[typing.Any]) -> typing.AsyncGenerator:
     """
-    Run provided functions in sequence. Yields values from functions
+    Run provided functions in sequence
+
+    Yields:
+        Any: return values from provided functions
     """
+
     for func in functions:
         yield await func
 
@@ -21,6 +26,7 @@ async def run_sequence(*functions: typing.Awaitable[typing.Any]):
     """
     Run provided functions in sequence
     """
+
     for func in functions:
         await func
 
@@ -28,5 +34,9 @@ async def run_sequence(*functions: typing.Awaitable[typing.Any]):
 async def run_parallel(*functions: typing.Awaitable[typing.Any]) -> tuple:
     """
     Run provided functions in parallel
+
+    Returns:
+        tuple: return values from provided functions
     """
+
     return await asyncio.gather(*functions)
