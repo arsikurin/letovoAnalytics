ff1195c8a17d8e184903c463aac7909ec3e112f5
arsikurin <2024kurin.av@student.letovo.ru>
2022-03-22 19:32:06 +0300

    Minor Release
    add rust pinger

--- app/pinger/main.rs
+++ app/pinger/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    let resp = reqwest::Client::new()
+        .get("https://letovo-analytics-api.herokuapp.com")
+        .send()
+        .await;
+
+    match resp {
+        Ok(..) => println!("Sent ping to API!"),
+        Err(e) => println!("Something went wrong!\n{}", e)
+    }
 }
