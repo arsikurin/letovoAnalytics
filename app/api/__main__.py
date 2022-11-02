36b49e8a56da31b1ba11aeed23e82c57d3a20d10
arsikurin <2024kurin.av@student.letovo.ru>
2022-11-02 10:47:08 +0300

    api errors redone minor fixes

--- app/api/__main__.py
+++ app/api/__main__.py
@@ -1,4 +1,5 @@
 import asyncio
+import http
 import logging as log
 from io import BytesIO
 
@@ -87,15 +88,22 @@ async def webcal_ics(user_id: int):
 
 @app.exception_handler(StarletteHTTPException)
 async def unknown_error(request: Request, error: HTTPException):
-    if hasattr(error, "headers"):
-        if error.headers is None:
-            error.headers = {}
+    if error.headers is None:
+        error.headers = {}
+
+    if error.headers.get("accept") == application_json:
         return ORJSONResponse(
-            {"code": error.status_code, "status": "error", "detail": error.detail, "fix": error.headers.get("fix")},
+            {"code": error.status_code, "status": "error", "detail": error.detail,
+             "fix": error.headers.get("fix") or ""},
             status_code=error.status_code
         )
-    return ORJSONResponse(
-        {"code": error.status_code, "status": "error", "detail": error.detail},
+
+    return templates.TemplateResponse(
+        error_page_t,
+        context={"request": request,
+                 "error": f"{error.status_code} {error.detail}",
+                 "detail": f"[{error.detail if error.detail != http.HTTPStatus(error.status_code).phrase else ''}]",
+                 "fix": f"{error.headers.get('fix') or ''}"},
         status_code=error.status_code
     )
 
@@ -105,75 +113,38 @@ async def validation_error(request: Request, error: ValidationError):
     return ORJSONResponse(error.json(), status_code=400)
 
 
-@app.exception_handler(502)
-async def bad_gateway(request: Request, error: HTTPException):
-    if error.headers is None:
-        error.headers = {}
-    if request.headers.get("accept") == application_json:
-        return ORJSONResponse(
-            {"code": error.status_code, "status": "error", "detail": error.detail, "fix": error.headers.get("fix")},
-            status_code=error.status_code
-        )
-    else:
-        return templates.TemplateResponse(
-            error_page_t,
-            context={"request": request,
-                     "error": "502 Bad Gateway",
-                     "detail": f"[{error.detail}]",
-                     "fix": f"{error.headers.get('fix')}"},
-            status_code=502
-        )
-
-
 @app.exception_handler(500)
 async def server_error(request: Request, error: HTTPException):
-    if isinstance(error, HTTPException):
-        if error.headers is None:
-            error.headers = {}
-        if request.headers.get("accept") == application_json:
-            return ORJSONResponse(
-                {"code": error.status_code, "status": "error", "detail": error.detail, "fix": error.headers.get("fix")},
-                status_code=error.status_code
-            )
-        else:
-            return templates.TemplateResponse(
-                error_page_t,
-                context={"request": request,
-                         "error": "500 Internal Server Error"},
-                status_code=500
-            )
+    log.error(f"{error.__repr__()=} {error.__doc__=}")
+
+    if hasattr(error, "headers") and error.headers is not None:
+        headers = error.headers
+    else:
+        headers = {}
+
+    if hasattr(error, "detail"):
+        detail = error.detail
+    else:
+        detail = http.HTTPStatus(500).phrase
+
+    fix = headers.get("fix") or ""
+
     if request.headers.get("accept") == application_json:
         return ORJSONResponse(
-            {"code": 500, "status": "error", "detail": error.__repr__(), "doc": error.__doc__},
-            status_code=500
-        )
-    else:
-        return templates.TemplateResponse(
-            error_page_t,
-            context={"request": request,
-                     "error": "500 Internal Server Error"},
+            {"code": 500, "status": "error", "detail": detail, "fix": fix},
             status_code=500
         )
 
-
-@app.exception_handler(422)
-async def unprocessable_entity(request: Request, error: HTTPException):
-    if error.headers is None:
-        error.headers = {}
-    if request.headers.get("accept") == application_json:
-        return ORJSONResponse(
-            {"code": error.status_code, "status": "error", "detail": error.detail, "fix": error.headers.get("fix")},
-            status_code=error.status_code
-        )
-    else:
-        return templates.TemplateResponse(
-            error_page_t,
-            context={"request": request,
-                     "error": "422 Unprocessable Entity",
-                     "detail": f"[{error.detail}]",  # [Possibly missing query string parameters]
-                     "fix": f"{error.headers.get('fix')}"},  # Use the link that bot provided
-            status_code=422
-        )
+    return templates.TemplateResponse(
+        error_page_t,
+        context={
+            "request": request,
+            "error": "500 Internal Server Error",
+            "detail": f"[{detail if detail != http.HTTPStatus(500).phrase else ''}]",
+            "fix": fix
+        },
+        status_code=500
+    )
 
 
 @app.exception_handler(418)
@@ -186,100 +157,6 @@ async def im_a_teapot(request: Request, error: HTTPException):
                  "fix": ""},
         status_code=418
     )
-
-
-@app.exception_handler(404)
-async def not_found(request: Request, error: HTTPException):
-    if request.headers.get("accept") == application_json:
-        if hasattr(error, "headers"):
-            return ORJSONResponse(
-                {"code": 404, "status": "error", "detail": error.detail, "fix": error.headers.get("fix")},
-                status_code=404
-            )
-        else:
-            return ORJSONResponse(
-                {"code": 404, "status": "error", "detail": error.detail},
-                status_code=404
-            )
-    else:
-        if error.detail != "Not Found":
-            return templates.TemplateResponse(
-                error_page_t,
-                context={"request": request,
-                         "error": "404 Not Found",
-                         "detail": f"[{error.detail}]"},
-                status_code=404
-            )
-        else:
-            return templates.TemplateResponse(
-                error_page_t,
-                context={"request": request,
-                         "error": "404 Not Found",
-                         "detail": "[The page you are looking for does not exist]"},
-                status_code=404
-            )
-
-
-@app.exception_handler(403)
-async def forbidden(request: Request, error: HTTPException) -> templates.TemplateResponse:
-    if error.headers is None:
-        error.headers = {}
-    if request.headers.get("accept") == application_json:
-        return ORJSONResponse(
-            {"code": error.status_code, "status": "error", "detail": error.detail, "fix": error.headers.get("fix")},
-            status_code=error.status_code
-        )
-    else:
-        return templates.TemplateResponse(
-            error_page_t,
-            context={"request": request,
-                     "error": "403 Forbidden",
-                     "detail": f"[{error.detail}]",  # [You have no power here, Gandalf the Grey]
-                     "fix": f"{error.headers.get('fix')}"},
-            status_code=403
-        )
-
-
-@app.exception_handler(401)
-async def unauthorized(request: Request, error: HTTPException) -> templates.TemplateResponse:
-    if error.headers is None:
-        error.headers = {}
-    if request.headers.get("accept") == application_json:
-        return ORJSONResponse(
-            {"code": error.status_code, "status": "error", "detail": error.detail, "fix": error.headers.get("fix")},
-            status_code=error.status_code
-        )
-    else:
-        return templates.TemplateResponse(
-            error_page_t,
-            context={"request": request,
-                     "error": "401 Unauthorized",
-                     "detail": f"[{error.detail}]",
-                     # [Possibly wrong credentials provided or your account is blocked]
-                     "fix": f"{error.headers.get('fix')}"},
-            # In case your account is blocked, contact Letovo Helpdesk
-            status_code=401
-        )
-
-
-@app.exception_handler(400)
-async def bad_request(request: Request, error: HTTPException) -> templates.TemplateResponse:
-    if error.headers is None:
-        error.headers = {}
-    if request.headers.get("accept") == application_json:
-        return ORJSONResponse(
-            {"code": error.status_code, "status": "error", "detail": error.detail, "fix": error.headers.get("fix")},
-            status_code=error.status_code
-        )
-    else:
-        return templates.TemplateResponse(
-            error_page_t,
-            context={"request": request,
-                     "error": "400 Bad Request",
-                     "detail": f"[{error.detail}]",
-                     "fix": f"{error.headers.get('fix')}"},
-            status_code=400
-        )
 
 
 if __name__ == "__main__":
